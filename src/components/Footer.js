b47e5803ef662336d0eff1f750d58cb228654119
Tevfik <tevfiksarlak@gmail.com>
2023-05-28 16:54:24 +0300

    components

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaInstagram, FaTwitter, FaGithub, FaPinterest } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="bg-green-700 h-120 mt-24">
+        <div className="flex flex-col md:grid md:grid-cols-3 mx-auto w-full py-4">
+          <div>
+            <div className="hidden md:block">
+              <a className="text-white font-semibold text-2xl md:text-4xl px-4 indent-1">
+                FR<span className="text-orange-500">ID</span>GE
+                <span className="text-orange-500">ZY</span>
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-white font-thin text-2xl md:text-4xl text-center md:text-left">
+              Let's Keep In Touch!
+            </h1>
+            <p className="text-white font-base text-lg md:text-base text-center md:text-left">
+              Find us on any of these platforms.
+            </p>
+
+            <div className="flex flex-row space-x-4 mx-auto md:ml-0">
+              <a href="#"><FaInstagram className="text-white text-2xl md:text-3xl hover:text-pink-700" /></a>
+              <a href="#"><FaTwitter className="text-white text-2xl md:text-3xl hover:text-sky-600" /></a>
+              <a href="#"><FaGithub className="text-white text-2xl md:text-3xl hover:text-violet-700" /></a>
+              <a href="#"><FaPinterest className="text-white text-2xl md:text-3xl hover:text-red-700" /></a>
+            </div>
+
+            <div className="md:pt-8 flex flex-col justify-center">
+                <img src="/images/bmc-button.png" alt="Buy Me a Coffee" className="h-8 w-24 mx-auto md:ml-0
+                         md:h-16 md:w-48 hover:scale-105" />
+            </div>
+         </div>
+          <div className="flex flex-col mx-auto md:ml-0 pt-4 md:pt-0 text-left">
+              <h1 className="text-xl md:text-2xl text-white font-thin underline mb-2 md:mb-4">RESOURCES</h1>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">Guide</a>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">Pricing</a>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">FAQ</a>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">Contact us</a>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">Licence</a>
+              <a className="text-base md:text-lg text-white font-thin mb-1 md:mb-2 hover:text-orange-500">Privacy Policy</a>
+
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+import LazyLoad from 'react-lazyload';
+
+
+
+export default function Intro(props) {
+  
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      const images = Array.from(document.querySelectorAll(".intro-image"));
+      const promises = images.map((image) => {
+        return new Promise((resolve) => {
+          image.onload = resolve;
+        });
+      });
+  
+      Promise.all(promises).then(() => {
+        setIsLoading(false);
+      });
+  
+      return () => {
+        images.forEach((image) => {
+          image.onload = null;
+        });
+      };
+    }, []);
+
+    if (isLoading) {
+        return (
+          <div className="flex items-center justify-center h-screen bg-white">
+            <p className="text-xl">Loading...🍕🍔🍟🥙🍗🥩</p>
+          </div>
+        );
+      }
+
+
+  return (
+    <div className="w-full mt-24">
+          <div className="flex flex-col md:grid md:grid-cols-2 space-y-4">
+            <div className="mx-auto ">
+              <LazyLoad>
+                 <img src="/images/Intro-one.jpg" 
+                     alt="Intro-one"
+                     className="h-96 drop-shadow-2xl intro-image rounded-md"
+                />     
+              </LazyLoad> 
+            </div>
+
+            <div className=" mx-auto font-carlito ">
+                 <h1  className="pt-16 text-4xl md:text-6xl text-slate-700 font-extrabold mb-6"
+                      >FR<span className="text-green-700">ID</span>GE<span className="text-orange-700">ZY</span></h1>
+                <p className=" text-2xl md:text-3xl text-slate-500 font-thin mb-12"
+                >"YOUR MEAL ADVISOR"</p>
+                <h3 className="text-green-700 text-2xl md:text-3xl font-semibold font-carlito mb-6"
+                >Just write what is in your fridge</h3>
+                <h1 className="text-xl md:text-2xl text-orange-700 font-semibold font-carlito"
+                >and see what you can cook</h1>
+            </div>
+
+        </div>
+
+        <div className="flex flex-col justify-center mx-auto md:grid md:grid-cols-2 mt-24  mb-8 ">
+            <div className="space-y-4 pt-24">
+                <h3 className="font-carlito text-2xl md:text-3xl text-slate-700 mx-auto font-semibold mb-8"
+                >Open app and write ingredients</h3>
+                <p className="ml-16 md:ml-32 text-left text-3xl md:text-4xl text-orange-400 font-thin mb-6"
+                >YOU WILL SEE:</p>
+                <ul className="list-disc text-slate-400  ml-8 md:ml-32 text-left  font-carlito">
+                    <li className="mb-6">what are your <span className="text-green-500 font-semibold">missing ingredienst</span> to cooking</li>
+                    <li className="mb-6">what are your <span className="text-orange-500 font-semibold">unused ingredienst</span></li>
+                    <li className="mb-6">And you can get <span className="text-slate-700 font-semibold">instructions</span></li>
+                </ul>
+                
+            </div>
+
+            <div>
+            <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 drop-shadow-2xl">
+                <div class="-m-1 flex flex-wrap md:-m-2">
+                    <div class="flex w-1/2 flex-wrap">
+                    <div class="w-1/2 p-1 md:p-2">
+                      <LazyLoad>
+                         <img
+                        alt="Intro-three"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-three.jpg" />
+                      </LazyLoad> 
+                    </div>
+                    <div class="w-1/2 p-1 md:p-2">
+                      <LazyLoad>
+                         <img
+                        alt="Intro-two"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-two.jpg" />
+                      </LazyLoad>   
+                    </div>
+                    <div class="w-full p-1 md:p-2">
+                      <LazyLoad>
+                        <img
+                        alt="Intro-four"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-four.jpg" />
+                      </LazyLoad>   
+                    </div>
+                    </div>
+                    <div class="flex w-1/2 flex-wrap">
+                    <div class="w-full p-1 md:p-2">
+                      <LazyLoad>
+                         <img
+                        alt="Intro-five"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-five.jpg" />
+                      </LazyLoad>   
+                    </div>
+                    <div class="w-1/2 p-1 md:p-2">
+                      <LazyLoad>
+                         <img
+                        alt="Intro-six"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-six.jpg" />
+                      </LazyLoad>  
+                    </div>
+                    <div class="w-1/2 p-1 md:p-2">
+                      <LazyLoad>
+                        <img
+                        alt="Intro-seven"
+                        class="block h-full w-full rounded-lg object-cover object-center intro-image"
+                        src="/images/Intro-seven.jpg" />
+                      </LazyLoad>        
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="mt-24 flex flex-col justify-center items-center">
+             <p className="text-2xl md:text-4xl font-thin font-carlito text-orange-700 mb-12"
+             >Let's click and ...</p>
+             <button onClick={() => props.handleStartButton()}
+                     className="bg-green-700 border-2 border-green-700 text-white font-semibold rounded-md px-24
+                                md:px-16 py-4  shadow-lg font-carlito
+                                hover:border-2 hover:border-orange-700 hover:text-orange-700 hover:bg-white
+                                hover:font-semibold active:bg-orange-300 active:duration-75 active:text-white"
+             >Find Your Meal</button>
+        </div>
+    </div>
+  )
+    
+}
