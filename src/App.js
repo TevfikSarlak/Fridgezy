239770bf60eeca9d5f4b6c20dcdcc38a35cfafe4
Tevfik <tevfiksarlak@gmail.com>
2023-05-29 12:51:36 +0300

    Lazy load was erased

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import SearchMeal from "./components/SearchMeal";
-import { useState, lazy, Suspense } from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Intro from "./components/Intro";
 
-const Intro = lazy(()=> import('./components/Intro'));
 
 
 function App() {
@@ -20,16 +20,12 @@ function App() {
       <Navbar startButton={startButton}
               setStartButton={setStartButton}
               handleStartButton={handleStartButton} />
-        <Suspense fallback={
-          <div className="flex items-center justify-center h-screen bg-white">
-          <p className="text-xl">Loading...🍕🍔🍟🥙🍗🥩</p>
-        </div>
-        } >
+        
        {startButton ? <SearchMeal /> 
        : <Intro startButton={startButton}
                 setStartButton={setStartButton}
                 handleStartButton={handleStartButton} />}
-        </Suspense>
+        
       <Footer />          
     </div>
   );
